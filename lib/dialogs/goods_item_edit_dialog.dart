cfd42a026097cebc60fc3048b83fda66c5f51366
Salykina Tatiana <salykina.tatiana@gmail.com>
2021-12-08 00:42:54 +0300

    [up] В окно просмотра и редактирования товара добавлено изображение товара и кнопка для его изменения.

--- lib/dialogs/goods_item_edit_dialog.dart
+++ lib/dialogs/goods_item_edit_dialog.dart
@@ -8,10 +8,11 @@ class GoodsItemEditDialog extends StatefulWidget {
   GoodsItem? goodsItem;
   VoidCallback onDataChanged;
 
-  GoodsItemEditDialog({Key? key, required this.onDataChanged, this.goodsItem}) : super(key: key);
+  GoodsItemEditDialog({Key? key, required this.onDataChanged, this.goodsItem})
+      : super(key: key);
 
   @override
-  _GoodsItemEditDialogState createState() => _GoodsItemEditDialogState(); // this.goodsItem
+  _GoodsItemEditDialogState createState() => _GoodsItemEditDialogState();
 }
 
 class _GoodsItemEditDialogState extends State<GoodsItemEditDialog> {
@@ -38,89 +39,117 @@ class _GoodsItemEditDialogState extends State<GoodsItemEditDialog> {
     bool isUpdateMode = widget.goodsItem != null;
     return Dialog(
       child: Container(
-        padding: const EdgeInsets.all(30),
+        padding: const EdgeInsets.all(15),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Add GoodsItem',
-              style: TextStyle(fontSize: 20),
-              textAlign: TextAlign.left,
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _titleTextController,
-              decoration: const InputDecoration(labelText: 'Item Name'),
-              onChanged: (String value) {
-                if(isUpdateMode) {
-                  widget.goodsItem!.title = _titleTextController.text;
-                  _updateGoodsItem2Database(widget.goodsItem!);
-                }
-                },
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Visibility(
-                      visible: isUpdateMode,
-                      child: ElevatedButton(
-                          child: const Text('Delete'),
-                          onPressed: () {
-                            _close();
-                            _deleteGoodsItem2Database(widget.goodsItem!);
-                          }
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Add GoodsItem',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.left,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                //mainAxisSize: MainAxisSize.min,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      width: 100,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image(
+                              image: AssetImage('assets/images/no_photo.jpg'),
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover
+                          ),
+                          ElevatedButton(
+                              child: const Text(
+                                'Change\nimage',
+                                maxLines: 2,
+                                textAlign: TextAlign.center,),
+                              onPressed: () {
+                                // change image
+                              })
+                        ],
                       )
-                    ),
-                    const SizedBox(width: 10),
-                  ]
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Visibility(
-                      visible: isUpdateMode,
-                      child: ElevatedButton(
-                        child: const Text('Close'),
-                        onPressed: () {
-                          _close();
-                        }
+                  ),
+                  //const SizedBox(width: 10),
+                  Expanded(
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: _titleTextController,
+                            decoration: const InputDecoration(labelText: 'Item Name'),
+                            onChanged: (String value) {
+                              if (isUpdateMode) {
+                                widget.goodsItem!.title = _titleTextController.text;
+                                _updateGoodsItem2Database(widget.goodsItem!);
+                              }
+                            },
+                          ),
+                        ],
                       )
-                    ),
+                  )
+                ],
+              ),
+              //const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
                     Visibility(
-                        visible: !isUpdateMode,
+                        visible: isUpdateMode,
                         child: ElevatedButton(
-                            child: const Text('Cancel'),
+                            child: const Text('Delete'),
                             onPressed: () {
                               _close();
-                            }
-                        )
-                    ),
-                    Visibility(
-                        visible: !isUpdateMode,
-                        child: const SizedBox(width: 10)
-                    ),
-                    Visibility(
-                      visible: !isUpdateMode,
-                      child: ElevatedButton(
-                          child: const Text('Add'),
-                          onPressed: () {
-                            newGoodsItem.title = _titleTextController.text;
-                            _addGoodsItem2Database(newGoodsItem);
-                            _close();
-                          }
-                      )
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ]
+                              _deleteGoodsItem2Database(widget.goodsItem!);
+                            })),
+                    const SizedBox(width: 10),
+                  ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Visibility(
+                          visible: isUpdateMode,
+                          child: ElevatedButton(
+                              child: const Text('Close'),
+                              onPressed: () {
+                                _close();
+                              })),
+                      Visibility(
+                          visible: !isUpdateMode,
+                          child: ElevatedButton(
+                              child: const Text('Cancel'),
+                              onPressed: () {
+                                _close();
+                              })),
+                      Visibility(
+                          visible: !isUpdateMode,
+                          child: const SizedBox(width: 10)),
+                      Visibility(
+                          visible: !isUpdateMode,
+                          child: ElevatedButton(
+                              child: const Text('Add'),
+                              onPressed: () {
+                                newGoodsItem.title = _titleTextController.text;
+                                _addGoodsItem2Database(newGoodsItem);
+                                _close();
+                              })),
+                    ],
+                  ),
+                ],
+              )
+            ]
         ),
       ),
     );
