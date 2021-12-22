b882a64d6797de9360c6390658c3b345abd50cf3
Andrey Kostin <andreykostin@inbox.ru>
2021-12-22 22:53:54 +0300

    collections++

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,31 +1,31 @@
 import 'package:consumer_basket/common/logger.dart';
 import 'package:consumer_basket/models/repository_item.dart';
 
-abstract class AbstractCollectionItem<ItemT>  {
-  AbstractCollection<ItemT>? collection;
-
-  final Logger _logger = Logger("CollectionItem<${ItemT.toString()}>");
-
-  Future<void> updateCollectionItem() async {
-    if(collection != null){
-      await collection!.updateItem(this as ItemT);
-    } else {
-      _logger.subModule("updateCollectionItem()").error("item does not have collection");
-    }
-  }
-
-  Future<void> removeCollectionItem() async {
-    if(collection != null){
-      await collection!.removeItem(this as ItemT);
-    } else {
-      _logger.subModule("removeCollectionItem()").error("item does not have collection");
-    }
-  }
-
-  // dynamic getId() {
-  //   _logger.subModule("getId()").error("abstract method is called");
-  // }
-}
+// abstract class AbstractCollectionItem<ItemT extends AbstractCollectionItem<ItemT> >  {
+//   AbstractCollection<ItemT>? collection;
+//
+//   final Logger _logger = Logger("CollectionItem<${ItemT.toString()}>");
+//
+//   Future<void> updateCollectionItem() async {
+//     if(collection != null){
+//       await collection!.updateItem(this as ItemT);
+//     } else {
+//       _logger.subModule("updateCollectionItem()").error("item does not have collection");
+//     }
+//   }
+//
+//   Future<void> removeCollectionItem() async {
+//     if(collection != null){
+//       await collection!.removeItem(this as ItemT);
+//     } else {
+//       _logger.subModule("removeCollectionItem()").error("item does not have collection");
+//     }
+//   }
+//
+//   // dynamic getId() {
+//   //   _logger.subModule("getId()").error("abstract method is called");
+//   // }
+// }
 
 
 abstract class AbstractCollectionParent {
@@ -79,44 +79,105 @@ abstract class AbstractCollection<ItemT> {
 
   final Logger _logger = Logger("AbstractCollection<${ItemT.toString()}>");
 
+  dynamic getItems() {
+    _logger.subModule("getItems()").warning("abstract method default implementation is called");
+  }
 
   Future<void> updateItem(ItemT item) async {
-
+    await updateItemImpl(item);
+    if(parent != null){
+      await parent!.collectionItemUpdated(this, item);
+    }else{
+      _logger.subModule("updateItem()").warning("collection dose not have parent");
+    }
   }
 
   Future<void> removeItem(ItemT item) async {
-    _logger.subModule("remove()").error("abstract method is called");
+    var logger = _logger.subModule("removeItem()");
+    // if(item.collection != this){
+    //   logger.error("removing failed: item dose not belong to this collection");
+    //   return;
+    // }
+    await removeItemImpl(item);
+    // item.collection = null;
+    if(parent != null){
+      await parent!.collectionItemRemoved(this, item);
+    }else{
+      logger.warning("collection dose not have parent");
+    }
+
   }
 
-  Future<void> updateItemImpl() async {
-    _logger.subModule("updateItemImpl()").warning("method default implementation is called");
+  Future<void> insertItem(ItemT item) async {
+    var logger = _logger.subModule("insertItem()");
+    // if(item.collection != null){
+    //   logger.error("insertion failed: item already belongs to other collection");
+    //   return;
+    // }
+    bool inserted = await insertItemImpl(item);
+    if(!inserted){
+      logger.error("insertion failed");
+      return;
+    }
+    // item.collection = this;
+    if(parent != null){
+      await parent!.collectionItemInserted(this, item);
+    }else{
+      logger.warning("collection dose not have parent");
+    }
   }
 
-  Future<void> removeItemImpl() async {
-    _logger.subModule("removeItemImpl()").warning("method default implementation is called");
+  Future<void> updateItemImpl(ItemT item) async {
+    _logger.subModule("updateItemImpl()").error("abstract method default implementation is called");
+  }
+
+  Future<void> removeItemImpl(ItemT item) async {
+    _logger.subModule("removeItemImpl()").error("abstract method default implementation is called");
+  }
+
+  Future<bool> insertItemImpl(ItemT item) async {
+    _logger.subModule("insertItemImpl()").error("abstract method default implementation is called");
+    return false;
   }
 }
 
 
+class RepositoryItemCollection<ItemT extends RepositoryItem> extends AbstractCollection<ItemT> {
+  Map<int, ItemT> _itemById = {};
+
+  final Logger _logger = Logger("RepositoryItemCollection<${ItemT.toString()}>");
+
+  @override
+  dynamic getItems() {
+    return _itemById;
+  }
+
+  @override
+  Future<void> updateItemImpl(ItemT item) async {
+    // do nothing
+  }
+
+  @override
+  Future<void> removeItemImpl(ItemT item) async {
+    _itemById.remove(item.id);
+  }
+
+  @override
+  Future<bool> insertItemImpl(ItemT item) async {
+    var logger = _logger.subModule("insertItemImpl()");
+    int? id = item.id;
+    if(id == null){
+      logger.error("item id is null");
+      return false;
+    }
+    if(_itemById.containsKey(id)){
+      logger.error("already contains item with id=$id");
+      return false;
+    }
+    _itemById[id] = item;
+    return true;
+  }
+}
 
 
-// class BaseTreeNode {
-//
-//   Future<void> childUpdated() async{
-//     Logger("BaseTreeNode").subModule("update()").error("abstract method is called");
-//   }
-// }
-//
-// class TreeNode<ChildItemT extends CollectionItem> extends BaseTreeNode {
-//   final Map<dynamic,ChildItemT> _items = {};
-//
-//   void add(ItemT){
-//
-//   }
-//
-//   @override
-//   Future<void> remove(ChildItemT item) async{
-//   }
-// }
-//
 
