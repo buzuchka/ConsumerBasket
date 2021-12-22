b5c91bf743b138d68cad4804485f6ef50de946b8
Andrey Kostin <andreykostin@inbox.ru>
2021-12-22 20:57:53 +0300

    collection++

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,36 +1,36 @@
 import 'package:consumer_basket/common/logger.dart';
 import 'package:consumer_basket/models/repository_item.dart';
 
-abstract class CollectionItem<ItemT>  {
+abstract class AbstractCollectionItem<ItemT>  {
   AbstractCollection<ItemT>? collection;
 
   final Logger _logger = Logger("CollectionItem<${ItemT.toString()}>");
 
   Future<void> updateCollectionItem() async {
     if(collection != null){
-      await collection!.update(this as ItemT);
+      await collection!.updateItem(this as ItemT);
     } else {
-      _logger.subModule("updateCollectionItem()").error("collection does not exist");
+      _logger.subModule("updateCollectionItem()").error("item does not have collection");
     }
   }
 
   Future<void> removeCollectionItem() async {
     if(collection != null){
-      await collection!.remove(this as ItemT);
+      await collection!.removeItem(this as ItemT);
     } else {
-      _logger.subModule("removeCollectionItem()").error("collection does not exist");
+      _logger.subModule("removeCollectionItem()").error("item does not have collection");
     }
   }
 
-  dynamic getId() {
-    _logger.subModule("getId()").error("abstract method is called");
-  }
+  // dynamic getId() {
+  //   _logger.subModule("getId()").error("abstract method is called");
+  // }
 }
 
 
-abstract class CollectionParent {
+abstract class AbstractCollectionParent {
 
-  Future<void> collectionNewItemInserted<ItemT>(
+  Future<void> collectionItemInserted<ItemT>(
       AbstractCollection<ItemT> collection,
       ItemT item) async {
   }
@@ -47,24 +47,27 @@ abstract class CollectionParent {
 }
 
 
-class CollectionSimpleRepoUpdater<ObjT> extends CollectionParent with RepositoryItem<ObjT> {
+class RepoCollectionParent<ObjT> extends AbstractCollectionParent with RepositoryItem<ObjT> {
 
-  Future<void> collectionNewItemInserted<ItemT>(
+  @override
+  Future<void> collectionItemInserted<ItemT>(
       AbstractCollection<ItemT> collection,
       ItemT item) async {
-    saveToRepository();
+    await saveToRepository();
   }
 
+  @override
   Future<void> collectionItemUpdated<ItemT>(
       AbstractCollection<ItemT> collection,
       ItemT item) async {
     // do nothing
   }
 
+  @override
   Future<void> collectionItemRemoved<ItemT>(
       AbstractCollection<ItemT> collection,
       ItemT item) async {
-    saveToRepository();
+    await saveToRepository();
   }
 }
 
@@ -72,16 +75,25 @@ class CollectionSimpleRepoUpdater<ObjT> extends CollectionParent with Repository
 abstract class AbstractCollection<ItemT> {
 
   String name = "Collection<${ItemT.toString()}>";
-
+  AbstractCollectionParent? parent;
 
   final Logger _logger = Logger("AbstractCollection<${ItemT.toString()}>");
 
-  Future<void> update(ItemT item) async {
-    _logger.subModule("update()").error("abstract method is called");
+
+  Future<void> updateItem(ItemT item) async {
+
   }
 
-  Future<void> remove(ItemT item) async {
+  Future<void> removeItem(ItemT item) async {
     _logger.subModule("remove()").error("abstract method is called");
+  }
+
+  Future<void> updateItemImpl() async {
+    _logger.subModule("updateItemImpl()").warning("method default implementation is called");
+  }
+
+  Future<void> removeItemImpl() async {
+    _logger.subModule("removeItemImpl()").warning("method default implementation is called");
   }
 }
 
