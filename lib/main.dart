0f23e4d59569ce01fb3d714cc81d79586ebf08d2
Salykina Tatiana <salykina.tatiana@gmail.com>
2022-01-02 04:40:56 +0300

    [move] Files moved from root.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:consumer_basket/base/logger.dart';
-import 'app.dart';
+
+import 'package:consumer_basket/widgets/app.dart';
 
 void main() {
   Logger().info("Start app");
