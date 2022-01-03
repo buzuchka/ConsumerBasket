f779fb9ac6a94f749b993edcd6c7ece0633a7173
Salykina Tatiana <salykina.tatiana@gmail.com>
2022-01-03 04:19:20 +0300

    [up] New screenshots for readme.

--- lib/core/helpers/constants.dart
+++ lib/core/helpers/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static const String appTitleString = "Consumer Basket";
-  static const String appVersionString = "0.0.1";
+  static const String appVersionString = "0.1.0";
 
   static const String viewDateFormatString = "dd.MM.yyyy";
   static String currentCurrencyString = "р.";
