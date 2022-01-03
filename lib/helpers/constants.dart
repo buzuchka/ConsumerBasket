6fe61ef766f3dd6611b6a6bdd965b6c80516c785
Salykina Tatiana <salykina.tatiana@gmail.com>
2022-01-03 03:13:26 +0300

    [up] Add comments.

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -7,10 +7,14 @@ class Constants {
   static const String viewDateFormatString = "dd.MM.yyyy";
   static String currentCurrencyString = "р.";
 
+  // Обычный размер картинки
   static const double pictureSize = 100.0;
+  // Размер картинки в элементе списка
   static const double listItemPictureSize = pictureSize;
 
+  // Высота элемента списка с картинкой
   static const double listItemPictureHeight = listItemPictureSize;
+  // Высота элемента списка без картинки
   static const double listItemNoPictureHeight = 70.0;
 
   static const double spacing = 10.0;
