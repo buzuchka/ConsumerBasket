a916bb3cd7abf2d097f3a644aad495095c414ac1
Salykina Tatiana <salykina.tatiana@gmail.com>
2022-01-24 23:39:51 +0300

    [up] Реализован поиск по названию в списке товаров.

--- lib/widgets/goods/goods.dart
+++ lib/widgets/goods/goods.dart
@@ -18,42 +18,99 @@ class GoodsScreen extends StatefulWidget {
 }
 
 class _GoodsScreenState extends State<GoodsScreen> {
+  final TextEditingController _filter = TextEditingController();
+
+  Icon _searchIcon = const Icon(Icons.search);
+  Widget _appBarTitle = const Text('');
+
+  late Future<List<GoodsItem>> _goodsListFuture;
+
+  _GoodsScreenState() {
+    _filter.addListener(() {
+      _rebuildScreen();
+    });
+  }
 
   @override
   void initState() {
     super.initState();
+
+    _goodsListFuture = _getAllGoods();
   }
 
   void _rebuildScreen() {
+    if(_filter.text.isEmpty) {
+      _goodsListFuture = _getAllGoods();
+    }
+    else {
+      _goodsListFuture = _getGoodsFiltered("${_filter.text}*");
+    }
     setState(() {});
+  }
+
+  Future<List<GoodsItem>> _getAllGoods() async {
+    return await RepositoriesHelper.goodsRepository.getAllOrdered();
+  }
+
+  Future<List<GoodsItem>> _getGoodsFiltered(String filterText) async {
+    return await RepositoriesHelper.goodsRepository.getByFts4QueryOrdered(filterText);
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (_searchIcon.icon == Icons.search) {
+        _searchIcon = const Icon(Icons.close);
+        _appBarTitle = TextField(
+          controller: _filter,
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              hintText: 'Search...'
+          ),
+        );
+      } else {
+        _searchIcon = const Icon(Icons.search);
+        _appBarTitle = const Text('');
+        _filter.clear();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: getListFutureBuilder(
-          RepositoriesHelper.goodsRepository.getAllOrdered(),
-          (GoodsItem item) => GoodsListItem(goodsItem: item),
-          onTap: editItemOnTap(
-            (GoodsItem item) => GoodsItemEditScreen(goodsItem: item),
-            () => _rebuildScreen(),
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            GoodsItem newGoodsItem = GoodsItem();
-            await RepositoriesHelper.goodsRepository.insert(newGoodsItem);
-            await Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => GoodsItemEditScreen(
-                      goodsItem: newGoodsItem)
-              ),
-            );
-            _rebuildScreen();
-          },
-          child: const Icon(Icons.add),
+      appBar: AppBar(
+        title: _appBarTitle,
+        actions: [
+          IconButton(
+            icon: _searchIcon,
+            onPressed: _searchPressed,
+          ),
+        ],
+      ),
+      body: getListFutureBuilder(
+        _goodsListFuture,
+        (GoodsItem item) => GoodsListItem(goodsItem: item),
+        onTap: editItemOnTap(
+          (GoodsItem item) => GoodsItemEditScreen(goodsItem: item),
+          () => _rebuildScreen(),
         )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          GoodsItem newGoodsItem = GoodsItem();
+          await RepositoriesHelper.goodsRepository.insert(newGoodsItem);
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => GoodsItemEditScreen(
+                    goodsItem: newGoodsItem)
+            ),
+          );
+          _filter.clear();
+          //_rebuildScreen();
+        },
+        child: const Icon(Icons.add),
+      )
     );
   }
 }
